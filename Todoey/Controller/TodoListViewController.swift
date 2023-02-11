8a296e7e9eede21c923bf5ea89cdba2b213d8cc5
seanka <and.seanka@gmail.com>
2023-02-11 21:18:35 +0700

    added clear search function

--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -97,5 +97,16 @@ extension ToDoListViewController: UISearchBarDelegate {
         
         loadItems(with: request)
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+           
+        }
+    }
 }
 
