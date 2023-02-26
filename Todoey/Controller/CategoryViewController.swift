00a7e9db2ac68b0227cf528bd4b00fdc69d85470
seanka <and.seanka@gmail.com>
2023-02-26 18:24:09 +0700

    added new table view screen and bind it to CategoryViewController

---
+++ Todoey/Controller/CategoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Sean Anderson on 26/02/23.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - TableView DataSource Methods
+    
+    // MARK: - Data Manipulate Methods
+    
+    // MARK: - Add New Categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    // MARK: - TableView Delegate Methods
+    
+}
