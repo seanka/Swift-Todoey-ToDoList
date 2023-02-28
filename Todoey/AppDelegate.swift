5c442d78174d6125d801e6eb47633bd9c33d0ee9
seanka <and.seanka@gmail.com>
2023-02-28 12:50:12 +0700

    Removed dead codes

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -14,10 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        print(Realm.Configuration.defaultConfiguration.fileURL!)
+//        print(Realm.Configuration.defaultConfiguration.fileURL!)
         
         do {
-            let realm = try Realm()
+            _ = try Realm()
         } catch {
             print("Error initializing realm \(error)")
         }
